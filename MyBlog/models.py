e1adaa0492f17cca9bd37742bb6214955361f4ad
ali_pzkk <man.pyyy@gmail.com>
2021-03-15 10:32:52 +0330

    MyBlog Add Category views

--- MyBlog/models.py
+++ MyBlog/models.py
@@ -5,12 +5,25 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime,date
 
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # return reverse('article-view', args=(str(self.id)))
+        return reverse('home')
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=260)
     title_tag = models.CharField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=260, default='open')
+
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
--- MyBlog/views.py
+++ MyBlog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 
 
@@ -25,6 +25,12 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = ('title', 'title_tag', 'author', 'body')
+
+class AddCategoryView(CreateView):
+    model = Category
+
+    template_name = 'add_category.html'
+    fields = ('__all__')
 
 
 class UpdatePostView(UpdateView):
