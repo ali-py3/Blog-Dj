6cb08faac61427a59e3f25a067d817b1d7a3953c
ali_pzkk <man.pyyy@gmail.com>
2021-03-24 16:52:06 +0430

    Add Rith Ckeditor

--- Blog_TSP/settings.py
+++ Blog_TSP/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'MyBlog',
-    'members'
+    'members',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
--- MyBlog/models.py
+++ MyBlog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
@@ -21,7 +22,7 @@ class Post(models.Model):
     title = models.CharField(max_length=260)
     title_tag = models.CharField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=260, default='open')
     likes = models.ManyToManyField(User, related_name='blog_post')
