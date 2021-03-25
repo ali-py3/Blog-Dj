da9fa2c2b8305696fa5dbdb2f384aacc398092fa
ali_pzkk <man.pyyy@gmail.com>
2021-03-25 07:43:32 +0430

    Editr Profile

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Register
+from .views import Register, UserEditeView
 
 urlpatterns = [
-    path('register/', Register.as_view(), name='register')
+    path('register/', Register.as_view(), name='register'),
+    path('edit_profile/', UserEditeView.as_view(), name='edit_profile')
 ]
