f13bb8cce2d4cc618626e5d736532a68e3687070
ali_pzkk <man.pyyy@gmail.com>
2021-03-28 13:45:42 +0430

    Edit Profile 2

--- MyBlog/models.py
+++ MyBlog/models.py
@@ -28,6 +28,13 @@ class Category(models.Model):
         return reverse('home')
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
 class Post(models.Model):
     title = models.CharField(max_length=260)
     title_tag = models.CharField(max_length=250)
